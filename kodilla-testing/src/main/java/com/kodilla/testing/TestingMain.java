616411719c23e3b37dd15a65725ed7777b475ec5
“MagdaToczek <kocik.magdalena@gmail.com>
2018-02-06 21:35:33 +0100

    Module 6.2 workout

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,34 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        Calculator calculator = new Calculator();
+
+        if (calculator.add(4,5) == (4 + 5)){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        if (calculator.subtract(4,5) == (4 - 5)){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
