b2a44c4f13a98d2690e7da464f10d6ccce676349
“MagdaToczek <kocik.magdalena@gmail.com>
2018-03-01 21:12:31 +0100

    9.1 - poprawione

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/ChallengesMain.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/ChallengesMain.java
@@ -5,13 +5,8 @@ import java.util.stream.Collectors;
 public class ChallengesMain {
     public static void main(String[] args) {
         MovieStore movieStore = new MovieStore();
-<<<<<<< HEAD
         String moviesListing = movieStore.getMovies().values().stream()
                 .flatMap(movies -> movies.stream())
-=======
-        String moviesListing = movieStore.getMovies().entrySet().stream()
-                .flatMap(movies -> movies.getValue().stream())
->>>>>>> 8331a66cb4e5f67d26296e07c623bbba78e2f127
                 .collect(Collectors.joining("!"));
 
         System.out.println(moviesListing);
