4ba956dc1b831116299a0be94b9d0c5e9e2162db
“MagdaToczek <kocik.magdalena@gmail.com>
2018-03-01 19:41:37 +0100

    9.1

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/ChallengesMain.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/ChallengesMain.java
@@ -1,10 +1,14 @@
 package com.kodilla.good.patterns.challenges;
 
+import java.util.stream.Collectors;
+
 public class ChallengesMain {
     public static void main(String[] args) {
         MovieStore movieStore = new MovieStore();
-        movieStore.getMovies().entrySet().stream()
-                .forEach(System.out::println);
+        String moviesListing = movieStore.getMovies().values().stream()
+                .flatMap(movies -> movies.stream())
+                .collect(Collectors.joining("!"));
 
+        System.out.println(moviesListing);
     }
 }
