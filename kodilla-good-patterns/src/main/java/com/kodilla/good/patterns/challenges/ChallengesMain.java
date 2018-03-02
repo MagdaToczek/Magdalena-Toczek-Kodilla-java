afcbefa0b34764ab47cd2b11aef5aff9b7dcca1c
“MagdaToczek <kocik.magdalena@gmail.com>
2018-03-02 18:26:13 +0100

    9.1 - wyrażenie lambda zamienione na referencję

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/ChallengesMain.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/ChallengesMain.java
@@ -1,12 +1,13 @@
 package com.kodilla.good.patterns.challenges;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class ChallengesMain {
     public static void main(String[] args) {
         MovieStore movieStore = new MovieStore();
         String moviesListing = movieStore.getMovies().values().stream()
-                .flatMap(movies -> movies.stream())
+                .flatMap(List::stream)
                 .collect(Collectors.joining("!"));
 
         System.out.println(moviesListing);
