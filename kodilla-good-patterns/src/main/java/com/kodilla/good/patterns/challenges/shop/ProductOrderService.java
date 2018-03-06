5226c5f4a40835ed67d822711d21df5ea7d4d6a9
“MagdaToczek <kocik.magdalena@gmail.com>
2018-03-06 17:59:26 +0100

    9.4 z poprawkami vol. 3

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/shop/ProductOrderService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/shop/ProductOrderService.java
@@ -10,11 +10,7 @@ public class ProductOrderService {
         Shop shopName = shopMap.get(orderRequest.getShopName());
         boolean isOrdered = shopName.process(orderRequest);
 
-        if(isOrdered) {
-            return new ShopDto(shopName, true);
-        }
-
-        return new ShopDto(shopName, false);
+        return new ShopDto(shopName, isOrdered);
     }
 
     public void addShop(String shopName, Shop shop) {
